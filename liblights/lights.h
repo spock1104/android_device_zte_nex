0957da1151d05ec2d8069ae128654e0713fa1f91
phenomx4 <phenomx49850@gmail.com>
2014-06-02 13:07:11 -0700

    Fix MMS and enable provided liblights

--- liblights/lights.h
+++ liblights/lights.h
@@ -5,7 +5,7 @@ char const*const RED_LED_FILE_TRIGGER           = "/sys/class/leds/red/blink";
 char const*const GREEN_LED_FILE_TRIGGER         = "/sys/class/leds/green/blink";
 
 char const*const BUTTON_BACKLIGHT_FILE[] = {
-  "/sys/class/leds/keyboard-backlight/brightness",
+  "/sys/class/leds/button-backlight/brightness",
 };
 
 char const*const LCD_BACKLIGHT_FILE	= "/sys/class/leds/lcd-backlight/brightness";
